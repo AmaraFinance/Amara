6cef82efff191f59215f0bd583a5921a89369fa3
liuleiCain <ll520cj1314>
2021-09-09 16:22:40 +0800

    token add lock

---
+++ contracts/Token/Context.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.8;
+
+contract Context {
+
+    constructor () internal { }
+
+    function _msgSender() internal view returns (address payable) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view returns (bytes memory) {
+        this;
+        return msg.data;
+    }
+}
---
+++ contracts/Token/Whitelist.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.5.16;
+
+import "./Ownable.sol";
+
+contract Whitelist is Ownable {
+    mapping(address => bool) whitelist;
+
+    event AddedToWhitelist(address indexed account);
+    event RemovedFromWhitelist(address indexed account);
+
+    modifier onlyWhitelisted() {
+        require(isWhitelisted(msg.sender));
+        _;
+    }
+
+    function add(address _address) public onlyOwner {
+        whitelist[_address] = true;
+        emit AddedToWhitelist(_address);
+    }
+
+    function remove(address _address) public onlyOwner {
+        whitelist[_address] = false;
+        emit RemovedFromWhitelist(_address);
+    }
+
+    function isWhitelisted(address _address) public view returns (bool) {
+        return whitelist[_address];
+    }
+}
