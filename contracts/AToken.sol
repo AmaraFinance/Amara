b3d5bd1e1a45ec25a7b043ef4a169323282d6176
liuleiCain <ll520cj1314>
2021-12-15 17:55:08 +0800

    Update AToken.sol

--- contracts/AToken.sol
+++ contracts/AToken.sol
@@ -686,6 +686,10 @@ contract AToken is ATokenInterface, Exponential, TokenErrorReporter {
         // EFFECTS & INTERACTIONS
         // (No safe failures beyond this point)
 
+        /* We write previously calculated values into storage */
+        totalSupply = vars.totalSupplyNew;
+        accountTokens[redeemer] = vars.accountTokensNew;
+
         /*
          * We invoke doTransferOut for the redeemer and the redeemAmount.
          *  Note: The aToken must handle variations between ERC-20 and ETH underlying.
@@ -693,10 +697,6 @@ contract AToken is ATokenInterface, Exponential, TokenErrorReporter {
          *  doTransferOut reverts if anything goes wrong, since we can't be sure if side effects occurred.
          */
         doTransferOut(redeemer, vars.redeemAmount);
-
-        /* We write previously calculated values into storage */
-        totalSupply = vars.totalSupplyNew;
-        accountTokens[redeemer] = vars.accountTokensNew;
 
         /* We emit a Transfer event, and a Redeem event */
         emit Transfer(redeemer, address(this), vars.redeemTokens);
@@ -778,18 +778,18 @@ contract AToken is ATokenInterface, Exponential, TokenErrorReporter {
         // EFFECTS & INTERACTIONS
         // (No safe failures beyond this point)
 
-        /*
-         * We invoke doTransferOut for the borrower and the borrowAmount.
-         *  Note: The aToken must handle variations between ERC-20 and ETH underlying.
-         *  On success, the aToken borrowAmount less of cash.
-         *  doTransferOut reverts if anything goes wrong, since we can't be sure if side effects occurred.
-         */
-        doTransferOut(borrower, borrowAmount);
-
         /* We write the previously calculated values into storage */
         accountBorrows[borrower].principal = vars.accountBorrowsNew;
         accountBorrows[borrower].interestIndex = borrowIndex;
         totalBorrows = vars.totalBorrowsNew;
+
+        /*
+        * We invoke doTransferOut for the borrower and the borrowAmount.
+        *  Note: The aToken must handle variations between ERC-20 and ETH underlying.
+        *  On success, the aToken borrowAmount less of cash.
+        *  doTransferOut reverts if anything goes wrong, since we can't be sure if side effects occurred.
+        */
+        doTransferOut(borrower, borrowAmount);
 
         /* We emit a Borrow event */
         emit Borrow(borrower, borrowAmount, vars.accountBorrowsNew, vars.totalBorrowsNew);
